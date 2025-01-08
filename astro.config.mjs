305b91239c6576573a4ae148b9b9ef80e18807fa
Rene Garcia <garciamx@gmail.com>
2025-01-08 15:45:47 -0600

    Landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://clubdeprogra.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
